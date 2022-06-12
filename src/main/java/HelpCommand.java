04098ec6185a9c0e7d9840dcb094fa9be0492ea5
agata_m <agata.malisz2@gmail.com>
2022-06-12 13:55:38 +0200

    help

--- src/main/java/HelpCommand.java
+++ src/main/java/HelpCommand.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class HelpCommand implements Command {
     @Override
     public void execute(String[] parameters) {
         System.out.println("Opcje, ktore mozesz wybrac");
-        for (String option : OptionToCommandMapper.commandMap.keySet()) {
-            System.out.println(option);
+
+        List<String> options = OptionToCommandMapper.commandMap.keySet().stream().toList();
+        List<String> modifiableList = new ArrayList<String>(options);
+
+        Collections.sort(modifiableList);
+
+        for (String o : modifiableList) {
+            System.out.println(o);
         }
     }
 }
