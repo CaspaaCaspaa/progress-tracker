c1b79b1b49cdb5fb25de302afc1f9716dff8f472
wkaras <wkaras@d17-427-pc-18.ipa.ki.agh.edu.pl>
2022-06-12 11:18:30 +0200

    Start class implementation

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Start {
-}
+import java.util.List;
+
+
+    public class Start implements Command {
+        @Override
+        public void execute(String projectName, String taskName) {
+            String timeStop = "";
+            Row row = new Row();
+            RowManager rowManager = new RowManager();
+            String timeStart = row.getTimeStop().toString();
+            rowManager.insertRow(projectName, taskName, timeStart, "dd-MM-yyyy HH:mm");
+
+
+        }
+
+
+    }
+
